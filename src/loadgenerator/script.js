6708c68389c50abd0ebfe3cc7ffa29b37a77e6d0
academey <academey@gmail.com>
2022-05-08 21:53:39 +0900

    Add prometheus scrape setting for search

--- src/loadgenerator/script.js
+++ src/loadgenerator/script.js
@@ -1,10 +1,10 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 
-export const options = {
-  vus: 10,
-  duration: '1s',
-};
+// export const options = {
+//   vus: 10,
+//   duration: '1s',
+// };
 
 export default function () {
   const res = http.get(__ENV.API_ENDPOINT);
