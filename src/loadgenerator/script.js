0b6798149f24662ebe723e7fa3f588c047df2a41
academey <academey@gmail.com>
2022-05-11 00:55:04 +0900

    Add nextjs commerce example repository

--- src/loadgenerator/script.js
+++ src/loadgenerator/script.js
@@ -7,11 +7,9 @@ import { sleep } from 'k6';
 // };
 
 export default function () {
-
-  http.get(checkout -> 장바구니 결제);
-  http.get(checkout -> 바로 결제);
-  http.get(광고 조회);
-
+  // http.get(checkout -> 장바구니 결제);
+  // http.get(checkout -> 바로 결제);
+  // http.get(광고 조회);
   const res = http.get(__ENV.API_ENDPOINT);
   sleep(1);
 }
