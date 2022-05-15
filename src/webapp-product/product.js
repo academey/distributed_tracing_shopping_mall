9466d4532ab14eab5ae6e15bfb656d212431ff91
academey <academey@gmail.com>
2022-05-15 21:38:28 +0900

    Revert "Change product source code"
    This reverts commit b0fa2cd150fdd6d251ccc9d5fe808b5ae34208f2.

--- src/webapp-product/product.js
+++ src/webapp-product/product.js
@@ -8,53 +8,51 @@ var app = express()
 app.use(cors());
 app.options('*', cors());
 
-var port = (process.env.PORT || '8007');
+var port = (process.env.PRODUCT_PORT || '8007');
 
 
-// 메일을 보내는 기능을 호출하기 위한 app.
-//만약 회원제(login system)이 있었다면, user id를 받아서 id에 해당하는 이메일 주소를 활용할 수 있을듯
-//반환객체 id: 메일의 고유 id, title: 메일 제목, receiver:메일 수신자
-//text: 메일 내용, date: 메일 보낸 날짜와 시간.
+// 물건의 세부 정보들을 반환해주기 위한 app
+//id: 상품 고유 id, title: 상품 이름, price: 상품 가격(기준은 USD), info:상품 정보
+//brand: 상품을 파는 회사, discount:상품할인에 의한 차감 비율(ex> 20%할인은 0.2로 표현)
+//추가 구현 가능성: 상품의 종류(가전제품, 식재료 등등...)
 
-/*
- 
-app.get('/email', function (req, res) {
-    const email= [{
-        id: 1,
-        title: 'search item test1'
-    },
-        {
-            id: 2,
-            title: 'search item test2'
-        }
-    ];
-    res.json(email);
+const products = [{
+    id: 1,
+    title: '몽키 바나나',
+    price: 3,  //가격은 항상 USD기준,
+    info: '바나나중에서도 최상급의 맛을 자랑하는 몽키 바나나! 빠른시간에 수입해와서 더 맛있어요',
+    brand: '(주)창설후르츠',
+    image: 'http://dnmart.co.kr/attach/main_img_4/79_202108_000_main.jpg',
+    discount: 0.2 //20%할인
+},
+    {
+        id: 2,
+        title: '게이밍 노트북',
+        price: 1700,  //가격은 항상 USD기준,
+        info: '가성비 좋은 게이밍 노트북, i7, rtx 3060 탑재',
+        brand: '(주)창설전자',
+        image: 'https://dimg.donga.com/wps/NEWS/IMAGE/2019/06/21/96106137.1.jpg',
+        discount: 0 //할인 없음
+    }
+];
+app.get('/product', function (req, res) {//기본 path는 모든 product 정보 반환
+    
+    res.json(products);
 });
 
-app.get('/email/:user_email@mail', function (req, res) {
-    const email = {
-        id: req.params.search_id,
-        title: 'search item test'
-    };
-    res.json(email);
-});*/
-
-//post 방식 사용 시도
-app.post('/email/', function(req, res){
-    //email 보내는 기능... 실제 구현은 못했음... 
-    var today = new Date();
-    console.log('이메일 보내기 성공!');//email 보내는 기능 대체로 콘솔로그...
-    const email = {
-        id: req.body.id,
-        title: req.body.title,
-        receiver: req.body.receiver,
-        text: req.body.text,
-        date: today
-
-    };
-})
+app.get('/product/:product_id', function (req, res) {
+    var id = parseInt(req.params.product_id, 10);
+    
+    if (isNaN(id) || id <= 0) {
+        res.status(400).send('Bad Request!')
+    } else if (products.length < id) {
+        res.status(404).send('Not Found!')
+    } else {
+        res.json(products[id - 1]);
+    }  
+});
 
 
 app.listen(port, function () {
-    console.log(`Example app listening on port 8004!`);
+    console.log(`Example app listening on port 8007!`);
 });
