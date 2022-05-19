a888efafef0a5107378d1f1f6fe97c334d547ea5
academey <academey@gmail.com>
2022-05-20 02:27:08 +0900

    Add k6 simple user scenario api testing

--- src/loadgenerator/script.js
+++ src/loadgenerator/script.js
@@ -1,15 +1,51 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
+import { check, group, sleep } from 'k6';
 
-// export const options = {
-//   vus: 10,
-//   duration: '1s',
-// };
+export const options = {
+  discardResponseBodies: true,
+  scenarios: {
+    contacts: {
+      executor: 'per-vu-iterations',
+      vus: 10,
+      iterations: 1,
+      maxDuration: '30s',
+    },
+  },
+};
+
+const SLEEP_DURATION = 0.1;
 
 export default function () {
   // http.get(checkout -> 장바구니 결제);
   // http.get(checkout -> 바로 결제);
   // http.get(광고 조회);
-  const res = http.get(__ENV.API_ENDPOINT);
-  sleep(1);
+  // const res = http.get("http://localhost:8008/cart");
+  // sleep(1);
+
+  group('cite3 shopping mall scenario', (_) => {
+    // Login request
+    const login_response = http.get('http://localhost:8008/cart', params);  
+    sleep(SLEEP_DURATION);
+
+    // Get user profile request
+    const user_profile_response = http.get(
+      'http://localhost:8008/cart',
+      params
+    );
+    sleep(SLEEP_DURATION);
+
+    // Update user profile request
+    params.tags.name = 'update-user-profile';
+    const update_profile_response = http.post(
+      'http://localhost:8008/cart',
+      params
+    );
+    sleep(SLEEP_DURATION);
+
+    // Logout request
+    params.tags.name = 'logout';
+    const logout_response = http.get('http://localhost:8008/cart', params);
+    sleep(SLEEP_DURATION);
+  });
+
 }
